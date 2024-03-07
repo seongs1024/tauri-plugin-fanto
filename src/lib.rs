b40011e86d44dbab0a18906943e06edcb56e4a98
Seongsu Park <56729421+seongs1024@users.noreply.github.com>
2024-03-07 13:33:59 +0900

    init webdriver process and destroy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use tauri::{
-  plugin::{Builder, TauriPlugin},
-  Manager, Runtime,
+	plugin::{Builder, TauriPlugin},
+	Manager, Runtime, RunEvent,
 };
 
 use std::{collections::HashMap, sync::Mutex};
@@ -28,29 +28,36 @@ struct MyState(Mutex<HashMap<String, String>>);
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the fanto APIs.
 pub trait FantoExt<R: Runtime> {
-  fn fanto(&self) -> &Fanto<R>;
+	fn fanto(&self) -> &Fanto<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> crate::FantoExt<R> for T {
-  fn fanto(&self) -> &Fanto<R> {
-    self.state::<Fanto<R>>().inner()
-  }
+	fn fanto(&self) -> &Fanto<R> {
+		self.state::<Fanto<R>>().inner()
+	}
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("fanto")
-    .invoke_handler(tauri::generate_handler![commands::execute])
-    .setup(|app, api| {
-      #[cfg(mobile)]
-      let fanto = mobile::init(app, api)?;
-      #[cfg(desktop)]
-      let fanto = desktop::init(app, api)?;
-      app.manage(fanto);
-
-      // manage state so it is accessible by the commands
-      app.manage(MyState::default());
-      Ok(())
-    })
-    .build()
+	Builder::new("fanto")
+	.invoke_handler(tauri::generate_handler![commands::execute])
+	.setup(|app, api| {
+		#[cfg(mobile)]
+		let fanto = mobile::init(app, api)?;
+		#[cfg(desktop)]
+		let fanto = desktop::Fanto::init(app, api)?;
+		app.manage(fanto);
+		
+		app.manage(MyState::default());
+		Ok(())
+	})
+	.on_event(|app, event| {
+		if let RunEvent::Exit = event {
+			if let Some(fanto) = app.try_state::<Fanto<R>>() {
+				#[cfg(desktop)]
+				fanto.destroy();
+			};
+		}
+	})
+	.build()
 }
